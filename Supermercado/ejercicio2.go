1af15800d9571b318ce94330d5ff8f6ea2c5a9d3
OriannaF <oriannafernandezdelrosario@ca.frre.utn.edu.ar>
2024-10-01 00:25:03 -0300

    parcial supermercado

---
+++ Supermercado/ejercicio2.go
@@ -0,0 +1,95 @@
+Accion Ej2 es
+Ambiente
+
+	movimientosDiarios = registro
+		clave = registro
+			productoId: N(5)
+		fr
+		clienteId: N(5)
+		tipo: "C","D" //compra devolucion
+		cantidad: N(4)
+		precioUnitario: N(10)
+		total: N(10)
+		tipoEnvio: "ENVIO","RETIRO"
+	fr
+
+	archMov: archivo de movimientosDiarios ordenado por clave
+	mov: movimientosDiarios
+
+	productos = registro
+		clave = registro
+			productoId: nombre(5)
+		fr
+		nombre: AN(40)
+		descripcion: AN(80)
+		rubro: "LIMPIEZA", "CARNICERIA", "VERDULERIA", "BAZAR", "PANADERIA"
+	fr
+
+	archProd: archivo de productos indexado por clave
+	prod: productos
+
+	a: arreglo de (1...6,1..3) de entero // i = rubro , j= envio
+	i,j,rubro,forma: entero
+
+	Funcion tipo(x:alfanumerico): entero es
+		Segun x hacer
+			"LIMPIEZA": rubro:= 1
+			"CARNICERIA": rubro:= 2
+			"VERDULERIA": rubro:= 3
+			"BAZAR": rubro:= 4
+			"PANADERIA": rubro:= 5
+		fs
+	fp
+
+	Funcion forma(x:alfanumerico): entero es
+		Segun x hacer
+			"C": forma:= 1
+			"D": rubro:= 2
+		fs
+	fp
+
+Algoritmo
+	AbrirE/(archMov) ; Abrir/S(archProd)
+	Para i=1 hasta 6 hacer
+		Para j=1 hasta 3 hacer
+			A(i,j):= 0
+		fp
+	fp
+
+	Mientras NFDA(mov) hacer
+		paq.productoId:= mov.productoId
+		tipo(paq.rubro)
+		forma(mov.tipo)
+		Leer(archProd,prod)
+		Si EXISTE entonces
+			A(rubro,forma):= A(rubro,forma) + mov.cantidad
+			A(rubro,3):= A(rubro,3) + mov.cantidad
+			A(6,forma):= A(6,forma) + mov.cantidad
+			A(6,3):= A(6,3) + mov.cantidad
+		fs
+	fm
+
+	mayor:= 0
+
+	Para i=1 hasta 5 hacer
+		Para j=1 hasta 2 hacer
+			Si A(i,j) > 200000 entonces
+				Esc("Para el rubro ", i, "de envio",j,"se tuvo un monto de" a
+			i,j)
+			fs
+
+			Si A(i,j) > mayor entonces
+				rubroMayor:= i
+				envioMayor:= j
+				
+			fs
+
+		fp
+	fp
+
+	Esc("Para el rubro ", i, "de envio",j,"se tuvo el mayor monto")
+
+	Esc("Se tuvo ", A(6;2)" devoluciones")
+
+
+fa
