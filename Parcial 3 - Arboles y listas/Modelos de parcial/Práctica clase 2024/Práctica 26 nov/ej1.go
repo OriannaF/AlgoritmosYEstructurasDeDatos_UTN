68253a29a7eb6b076c967f692a9c5d3e2630e930
Orianna <oriannafernandezdelrosario@ca.frre.utn.edu.ar>
2024-11-28 21:24:06 -0300

    listas

--- Parcial 3 - Arboles y listas/Modelos de parcial/Práctica clase 2024/Práctica 26 nov/ej1.go
+++ Parcial 3 - Arboles y listas/Modelos de parcial/Práctica clase 2024/Práctica 26 nov/ej1.go
@@ -23,10 +23,57 @@ Ambiente
 	//listas de salida (ambas dobles)
 
 	//La primera contendrá el código y nombre de los proyectos que tengan todos sus errores en estado resueltos
-	//osea, Si todos los errores dan *p.estado = "R" entonces se pone en la lista: proy.clave.cod proy.nombre
+	//tengo que sacar los errores, ir viendo en la lista con la cantidad de errores y verificar si estan resueltos
 
 	//La segunda contendrá el código y nombre de los proyectos que tengan al menos el 50% de sus errores en estado resueltos o en Proceso. (Ordenada por dicho porcentaje de mayor a menor)
 
 
 Algoritmo
+
+	AbrirE/(archProy) ; Leer(archProy,proy)
+
+	//inicializar
+	contador:= 0 ; resuelto:= 0 ; noResuelto:= 0; proyectosNoResueltos:= 0
+
+	p:= PRIM
+
+	mientras NFDA(archProy) hacer
+
+		erroresTotales:= proy.cantErrores
+		contador:= proy.cantErrores
+
+		mientras (contador <> 0) hacer
+			si (*p.estado = "R") entonces
+				resuelto:= resuelto + 1
+			sino
+				si (*p.estado = "N") entonces //última actividad (hay que informar)
+					noResuelto:= noResuelto + 1
+				sino //en proceso
+					enProceso:= enProceso + 1
+				fs
+			fs
+			p:= *p.prox
+			contador:= contador - 1
+		fm
+
+		//primera lista
+		si (erroresTotales = resuelto) entonces 
+			//pongo en primer lista cod y nom del archivo
+		fs
+
+		//segunda lista
+		si ((((resuelto + proceso)/erroresTotales)* 100) > 50 ) entonces
+			//carga ordena descendente
+		fs
+
+		//última consigna (se informa dsp)
+		si (erroresTotales = noResuelto) entonces
+			proyectosNoResueltos:= proyectosNoResueltos + 1
+		fs
+		
+	fm
+
+	esc("la cantidad de proyectos no resueltos es de: ", proyectosNoResueltos)
+
+	cerrar(archProy)
 fa
