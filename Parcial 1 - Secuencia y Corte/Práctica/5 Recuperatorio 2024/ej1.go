9c0ed9ef9170b9fd231d1d3ba4d0f0574fd5ffc3
Orianna <139834490+OriannaF@users.noreply.github.com>
2025-02-26 10:32:32 -0300

    act

--- Parcial 1 - Secuencia y Corte/Práctica/5 Recuperatorio 2024/ej1.go
+++ Parcial 1 - Secuencia y Corte/Práctica/5 Recuperatorio 2024/ej1.go
@@ -9,11 +9,12 @@
 
 Accion ej1 es 
 Ambiente
-	secHistorial: secuencia de caracteres
+	historial: secuencia de caracteres
 	h,d: caracter
-	secDeportistas: secuencia de caracteres
+	deportes: secuencia de caracteres
+	igualan, superan
 
-	funcion covertir(d: caracter): entero
+	funcion covertir(d: caracter): entero // no estoy segura de esta funcion tt
 		segun d hacer
 			"1": convertir := 1
 			"2": convertir := 2
@@ -37,20 +38,58 @@ Ambiente
 		edad:= convertir(d) * 10
 		Avz(deportes,d)
 		edad:= edad + convertir(d)
+
+		si (edad > edadUsuario) entonces
+			superan:= superan + 1
+		sino
+			si (edad = edadUsuario) entonces
+				igualan:= igualan + 1 
+			fs
+		fs
 	fp
 
+	procedimiento salidaPais() es
+		Repetir
+			Esc(salida,h) ; Avz(historial,h)
+		hasta h = "#"
+	fp
+
+	procedimiento saltarDeporteYVerificar() es
+		Avz(deportes,d)
+		si (d EN vocales) entonces
+			Avz(deportes,d) ; Avz(deportes,d)
+			deporte:= true
+		fs	
+	fp
+
+	procedimiento salidaAtleta() es
+		Repetir
+			Esc(salida,d) ; Avz(deporte,d)
+		hasta h = "#"
+		Avz(deporte,d)
+	fp
+
+	procedimiento saltarAtleta() es
+		Repetir
+			Avz(deporte,d)
+		hasta d = "#"
+	fp
+
+	
 Algoritmo
 
 	Iniciar() ; Crear(salida) 
 
-	si (historial = "P") entonces
-		salidaPais() ; Esc(salida,"#")
-		saltarDeporteYVerificar() ; edadAtleta() 
-		si (deporte EN vocales) entonces
-			salidaAtleta() ; Esc(salida,"%")
-		sino
-			saltarAtleta()
+	mientras NFDS(historial) hacer
+		si (historial = "P") entonces
+			salidaPais() ; Esc(salida,"#")
+			saltarDeporteYVerificar() ; edadAtleta() 
+			si (deporte = true) entonces
+				salidaAtleta() ; Esc(salida,"%")
+			sino
+				saltarAtleta()
+			fs
 		fs
-	fs
+	fm
 
 Fa
